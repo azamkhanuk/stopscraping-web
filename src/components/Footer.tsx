076f1f43d3d4db3fb52fd1e9cf5aa9fd5bec8ea8
Azam Khan <coding@azamkhan.co.uk>
2024-09-09 00:17:21 +0100

    minor refactor

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -3,7 +3,9 @@ import { getCurrentYear } from "@/utils/dateUtils"
 export const Footer = () => {
     return (
         <footer className="container mx-auto px-4 py-6 text-center text-gray-500 text-sm">
-            <p>&copy; {getCurrentYear()} | stopscraping.me | Might save you from a big server bill :)</p>
+            <p>&copy; {getCurrentYear()} | stopscraping.me |
+                <a href="mailto:contact@stopscraping.me" className="text-blue-500 hover:underline"> contact@stopscraping.me</a>
+            </p>
         </footer>
     )
 }
