25bf14ce7c2c0c1d56efb15bdd6200fe4c7380fb
Azam Khan <coding@azamkhan.co.uk>
2024-09-08 02:09:12 +0100

    more config

--- api/create-checkout-session.ts
+++ api/create-checkout-session.ts
@@ -2,7 +2,7 @@ import { VercelRequest, VercelResponse } from '@vercel/node';
 import Stripe from 'stripe';
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
-    apiVersion: '2023-10-16',
+    apiVersion: '2024-06-20',
 });
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
