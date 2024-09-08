eec2646928a87da404995070b37602d524baa186
Azam Khan <coding@azamkhan.co.uk>
2024-09-08 02:28:48 +0100

    names again

--- api/verify-payment.ts
+++ api/verify-payment.ts
@@ -1,7 +1,7 @@
 import { VercelRequest, VercelResponse } from '@vercel/node';
 import Stripe from 'stripe';
 
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!, {
+const stripe = new Stripe(process.env.VITE_STRIPE_SECRET_KEY!, {
     apiVersion: '2024-06-20',
 });
 
