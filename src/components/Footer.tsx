b5a1b1ec9707d3bd61c0c0a047450b557783da22
Azam Khan <coding@azamkhan.co.uk>
2025-07-16 00:38:21 +0100

    remove mail to link

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,21 +1,21 @@
 import { Link } from 'react-router-dom';
-import { getCurrentYear } from "@/utils/dateUtils"
+import { getCurrentYear } from '@/utils/dateUtils';
 
 export const Footer = () => {
-    return (
-        <footer className="container mx-auto px-4 py-6 text-center text-gray-500 text-sm">
-            <p className="mb-2">&copy; {getCurrentYear()} | stopscraping.me</p>
-            <nav className="space-x-4">
-                <Link to="/privacy-policy" className="text-gray-400 hover:text-white transition-colors">
-                    Privacy Policy
-                </Link>
-                <Link to="/terms-and-conditions" className="text-gray-400 hover:text-white transition-colors">
-                    Terms and Conditions
-                </Link>
-                <a href="mailto:contact@stopscraping.me" className="text-gray-400 hover:text-white transition-colors">
-                    contact@stopscraping.me
-                </a>
-            </nav>
-        </footer>
-    )
-}
+  return (
+    <footer className='container px-4 py-6 mx-auto text-sm text-center text-gray-500'>
+      <p className='mb-2'>&copy; {getCurrentYear()} | stopscraping.me</p>
+      <nav className='space-x-4'>
+        <Link to='/privacy-policy' className='text-gray-400 transition-colors hover:text-white'>
+          Privacy Policy
+        </Link>
+        <Link
+          to='/terms-and-conditions'
+          className='text-gray-400 transition-colors hover:text-white'
+        >
+          Terms and Conditions
+        </Link>
+      </nav>
+    </footer>
+  );
+};
